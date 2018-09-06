971d934c5472e858054acab33d5f1cc5483fed12
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-09-06 11:31:40 +0200

    Commit del 6 settembre 2018 ore 11:31

--- bmi_test/src/bmi_test.c
+++ bmi_test/src/bmi_test.c
@@ -10,6 +10,7 @@
 
 #include <CUnit/basic.h>
 #include "print_results/bmi/bmi.h"
+#include "is_valid_sex/is_valid_sex.h"
 
 int init_suite_default(){
 	return 0;
@@ -19,11 +20,26 @@ int clear_suite_default(){
 	return 0;
 }
 
+void test_is_valid_sex(){
+	CU_ASSERT_TRUE(is_valid_sex('F'));
+	CU_ASSERT_TRUE(is_valid_sex('M'))
+	CU_ASSERT_FALSE(is_valid_sex('f'));
+	CU_ASSERT_FALSE(is_valid_sex('m'));
+	CU_ASSERT_FALSE(is_valid_sex('A'));
+	CU_ASSERT_FALSE(is_valid_sex('C'));
+	CU_ASSERT_FALSE(is_valid_sex('L'));
+	CU_ASSERT_FALSE(is_valid_sex('N'));
+	CU_ASSERT_FALSE(is_valid_sex('a'));
+	CU_ASSERT_FALSE(is_valid_sex('c'));
+	CU_ASSERT_FALSE(is_valid_sex('l'));
+	CU_ASSERT_FALSE(is_valid_sex('n'));
+}
+
 void test_bmi(){
 	CU_ASSERT_EQUAL((int)bmi(1.68,88),31);
 	CU_ASSERT_EQUAL((int)bmi(1.68,82),29);
 	CU_ASSERT_EQUAL((int)bmi(1.68,72),25);
-	CU_ASSERT_EQUAL((int)bmi(1.68,50),17);
+	CU_ASSERT_EQUAL((int)bmi(2.00,64),16);
 }
 
 void test_corporature(){
@@ -45,13 +61,15 @@ void test_underweight(){
 
 int main(void) {
 	CU_initialize_registry();
-	CU_pSuite pSuite_A = CU_add_suite("SUITE BMI",init_suite_default,clear_suite_default);
-	CU_add_test(pSuite_A,"test of bmi()",test_bmi);
-	CU_pSuite pSuite_B = CU_add_suite("SUITE CORP",init_suite_default,clear_suite_default);
-	CU_add_test(pSuite_B,"test of corporature()",test_corporature);
-	CU_pSuite pSuite_C = CU_add_suite("SUITE OVER-UNDERWEIGHT",init_suite_default,clear_suite_default);
-	CU_add_test(pSuite_C,"test of overweight()",test_overweight);
-	CU_add_test(pSuite_C,"test of underweight()",test_underweight);
+	CU_pSuite pSuite_A = CU_add_suite("SUITE VALID SEX",init_suite_default,clear_suite_default);
+	CU_add_test(pSuite_A,"test of is_valid_sex()",test_is_valid_sex);
+	CU_pSuite pSuite_B = CU_add_suite("SUITE BMI",init_suite_default,clear_suite_default);
+	CU_add_test(pSuite_B,"test of bmi()",test_bmi);
+	CU_pSuite pSuite_C = CU_add_suite("SUITE CORP",init_suite_default,clear_suite_default);
+	CU_add_test(pSuite_C,"test of corporature()",test_corporature);
+	CU_pSuite pSuite_D = CU_add_suite("SUITE OVER-UNDERWEIGHT",init_suite_default,clear_suite_default);
+	CU_add_test(pSuite_D,"test of overweight()",test_overweight);
+	CU_add_test(pSuite_D,"test of underweight()",test_underweight);
 	CU_basic_set_mode(CU_BRM_VERBOSE);
 	CU_basic_run_tests();
 	CU_cleanup_registry();
